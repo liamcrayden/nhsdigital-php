cffd538ce2e6ab2b78f76185b02990619ffe9675
Liam Crayden <liam@workingfeedback.co.uk>
2020-08-12 14:57:11 +0100

    InvalidCommentIDException typo

--- src/Exceptions/InvalidCommentIDException.php
+++ src/Exceptions/InvalidCommentIDException.php
@@ -21,7 +21,7 @@ class InvalidCommentIDException extends Exception
     public function __construct($commentID, $code = 0, Exception $previous = null)
     {
 
-        $this->message = 'The comment ID "' . $subscriptionKey . '" is not valid.' . PHP_EOL;
+        $this->message = 'The comment ID "' . $commentID . '" is not valid.' . PHP_EOL;
         parent::__construct( $this->message, $code, $previous );
 
     }
